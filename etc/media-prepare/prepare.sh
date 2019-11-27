57980881af296147cc242e941b0952d5f88ecf10
Miyamoto Kazuyuki <k-miyamoto@pu.jp.nec.com>
2019-11-27 17:30:45 +0900

    Create prepare.sh

---
+++ etc/media-prepare/prepare.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#export https_proxy=http://PROXY_SERVER:PORT/
+#export http_proxy=http://PROXY_SERVER:PORT/
+
+# HAUC
+curl -O https://codeload.github.com/mkazuyuki/hauc/zip/master
+unzip master
+rm master
+
+# EC
+curl -O https://www.nec.com/en/global/prod/expresscluster/en/trial/zip/ecx41l_x64.zip
+unzip ecx41l_x64.zip
+cp ecx41l_x64/Linux/4.1/en/server/expresscls-4.1.1-1.x86_64.rpm hauc-master/cf/
+
+##
+## Preparing trial license files is needed
+##
+cp ECX4.x-lin1.key     hauc-master/cf/
+cp ECX4.x-Rep-lin1.key hauc-master/cf/
+cp ECX4.x-Rep-lin2.key hauc-master/cf/
+
+rm -rf ecx41l_x64
+rm ecx41l_x64.zip
+
+# PUTTY
+cd hauc-master/cf
+curl -O https://the.earth.li/~sgtatham/putty/latest/w64/putty.exe
+curl -O https://the.earth.li/~sgtatham/putty/latest/w64/plink.exe
+curl -O https://the.earth.li/~sgtatham/putty/latest/w64/pscp.exe
+curl -O http://strawberryperl.com/download/5.30.0.1/strawberry-perl-5.30.0.1-64bit.msi
+curl -O http://archive.kernel.org/centos-vault/7.6.1810/isos/x86_64/CentOS-7-x86_64-DVD-1810.iso
+cd ../..
